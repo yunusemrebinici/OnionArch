b9d3df0a269822c92638b9ca2982090cced2ca2f
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-15 15:54:31 +0300

    .

--- Persistance/Context/CarBookContext.cs
+++ Persistance/Context/CarBookContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,27 @@ using System.Threading.Tasks;
 
 namespace Persistance.Context
 {
-	public class CarBookContext
+	public class CarBookContext:DbContext
 	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=EMRE\\SQLEXPRESS01;initial Catalog=CarBook;integrated Security=true;");
+		}
+		public DbSet<About> Abouts { get; set; }
+		public DbSet<Banner> Banners { get; set; }
+		public DbSet<Brand> Brands { get; set; }
+		public DbSet<Car> Cars { get; set; }
+		public DbSet<CarDescription> CarDescriptions { get; set; }
+		public DbSet<CarFeature> CarFeatures { get; set; }
+		public DbSet<CarPricing> CarPricings { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Contact> Contacts { get; set; }
+		public DbSet<Feature> Features { get; set; }
+		public DbSet<FooterAddress> FooterAddresses { get; set; }
+		public DbSet<Location> Locations { get; set; }
+		public DbSet<Pricing> Pricings { get; set; }
+		public DbSet<Services> MyProperty { get; set; }
+		public DbSet<SocialMedia> SocialMedias { get; set; }
+		public DbSet<Testimonial> Testimonials { get; set; }
 	}
 }

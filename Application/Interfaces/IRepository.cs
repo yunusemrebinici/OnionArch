38173fcd3f95aa539d8de7de86030157af7873ea
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-15 16:38:15 +0300

    .

---
+++ Application/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+	public interface IRepository<T> where T : class
+	{
+		Task<List<T>> GettAllAsync();
+
+		Task<T> GetByIdAsync(int id);
+
+		Task CreateAsync(T t);
+
+		Task UpdateAsync(T t);
+
+		Task RemoveAsync(T t);
+
+	}
+}

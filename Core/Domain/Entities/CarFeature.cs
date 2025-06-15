24cd5f369eef72f925618eca89b22c58b4a23024
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-15 13:13:38 +0300

    .

---
+++ Core/Domain/Entities/CarFeature.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+	public class CarFeature
+	{
+		public int CareFaatureID { get; set; }
+
+		public int CarID { get; set; }
+
+		public Car Car { get; set; }
+
+		public Feature Feature { get; set; }
+
+		public int FeatureID { get; set; }
+
+		public bool Available { get; set; }
+	}
+}

1744ee89f7a1dc6b17ab5f5b2bafb00a3baa4e80
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-16 12:37:43 +0300

    .

---
+++ Application/Features/CQRS/Handlers/AboutHandlers/UpdateAboutCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.CQRS.Commands.AboutCommands;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Handlers.AboutHandlers
+{
+	public class UpdateAboutCommandHandler
+	{
+		private readonly IRepository<About> _repository;
+
+		public UpdateAboutCommandHandler(IRepository<About> repository)
+		{
+			_repository = repository;
+		}
+		
+		public async Task Handle(UpdateAboutCommand command)
+		{
+			await _repository.UpdateAsync(new About
+			{
+				AboutID = command.AboutID,
+				Description = command.Description,
+				ImageUrl = command.ImageUrl,
+				Title = command.Title	
+			});
+		}
+	}
+}

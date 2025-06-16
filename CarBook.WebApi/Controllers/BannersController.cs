ef7746c80245abe5887bc28acba08d5fea340015
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-16 15:44:04 +0300

    .

---
+++ CarBook.WebApi/Controllers/BannersController.cs
@@ -0,0 +1,69 @@
+﻿using Application.Features.CQRS.Commands.BannerCommands;
+using Application.Features.CQRS.Handlers.BannerHandlers;
+using Application.Features.CQRS.Queries.BannerQuaries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BannersController : ControllerBase
+	{
+		private readonly CreateBannerCommandHandler _createBanner;
+		private readonly GetBannerQueryHandler _getBannerQuery;
+		private readonly GetBannerByIdQueryHandler _getBannerByIdQuery;
+		private readonly RemoveBannerCommandHandler _removeBanner;
+		private readonly UpdateBannerCommandHandler _updateBanner;
+
+		public BannersController(
+			CreateBannerCommandHandler createBanner,
+			GetBannerQueryHandler getBannerQuery,
+			GetBannerByIdQueryHandler getBannerByIdQuery,
+			RemoveBannerCommandHandler removeBanner,
+			UpdateBannerCommandHandler updateBanner)
+		{
+			_createBanner = createBanner;
+			_getBannerQuery = getBannerQuery;
+			_getBannerByIdQuery = getBannerByIdQuery;
+			_removeBanner = removeBanner;
+			_updateBanner = updateBanner;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> BannerList()
+		{
+			return Ok(await _getBannerQuery.Handle());
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult>GetBanner(int id)
+		{
+			return Ok(await _getBannerByIdQuery.Handle(new GetBannerByIdQuary(id)));
+		}
+
+		[HttpPost]
+		public async Task<IActionResult>CreateBanner(CreateBannerCommand command)
+		{
+	        await _createBanner.Handle(command);
+			return Ok("Banner Ekleme Başarılı");
+		}
+
+		[HttpPut]
+		public async Task<IActionResult> UpdateBanner(UpdateBannerCommand command)
+		{
+			await _updateBanner.Handle(command);
+			return Ok("Güncelleme Başarılı");
+		}
+
+		[HttpDelete]
+		public async Task<IActionResult> RemoveBanner(RemoveBannerCommand command)
+		{
+			await _removeBanner.Handle(command);
+			return Ok("Silme İşlemi Başarılı"); 
+			
+		}
+
+
+	}
+}
--- CarBook.WebApi/Program.cs
+++ CarBook.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Application.Features.CQRS.Handlers.AboutHandlers;
+using Application.Features.CQRS.Handlers.BannerHandlers;
 using Application.Interfaces;
 using Persistance.Context;
 using Persistance.Repositories;
@@ -7,12 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+
 builder.Services.AddScoped<CarBookContext>();
+
 builder.Services.AddScoped<CreateAboutCommandHandler>();
 builder.Services.AddScoped<GetAboutByIdQueryHandler>();
 builder.Services.AddScoped<GetAboutQueryHandler>();
 builder.Services.AddScoped<RemoveAboutCommandHandler>();
 builder.Services.AddScoped<UpdateAboutCommandHandler>();
+
+builder.Services.AddScoped<CreateBannerCommandHandler>();
+builder.Services.AddScoped<GetBannerByIdQueryHandler>();
+builder.Services.AddScoped<GetBannerQueryHandler>();
+builder.Services.AddScoped<RemoveBannerCommandHandler>();
+builder.Services.AddScoped<UpdateBannerCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

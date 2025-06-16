10c3346a43650650e4ff3c05aa5086217cda3a38
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-16 11:53:20 +0300

    .

---
+++ Application/Features/CQRS/Commands/AboutCommands/CreateAboutCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Commands.AboutComment
+{
+	public class CreateAboutCommand
+	{
+		
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public string ImageUrl { get; set; }
+	}
+}

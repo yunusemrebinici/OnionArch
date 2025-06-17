a8f56f365aee3110487c3ff4d241e7791ed5e7ba
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-17 14:06:12 +0300

    .

---
+++ Application/Features/CQRS/Queries/CarQuaries/GetCarByIdQuaries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Queries.CarQuaries
+{
+	public class GetCarByIdQuaries
+	{
+		public GetCarByIdQuaries(int id)
+		{
+			Id = id;
+		}
+		public int Id { get; set; }
+	}
+}

993d5a8d62c52f83e6d4901bb9ab38d29eaac3a7
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-19 17:15:15 +0300

    .

---
+++ CarBook.WebApi/Controllers/FooterAddressController.cs
@@ -0,0 +1,53 @@
+﻿using Application.Features.Mediator.Commands.FooterAddressCommands;
+using Application.Features.Mediator.Quaries.FooterAddressQuaries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FooterAddressController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public FooterAddressController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> FooterAddressList()
+		{
+			return Ok(await _mediator.Send(new GetFooterAddressQuery()));
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetFooterAddress(int id)
+		{
+			return Ok( await _mediator.Send(new GetFooterAddressByIdQuery(id)));
+		}
+
+		[HttpPost]
+		public async Task<IActionResult>CreateFooterAddress(CreateFooterAdressCommand command)
+		{
+			await _mediator.Send(command);
+			return Ok("Ekleme Başarılı");
+		}
+
+		[HttpPut]
+		public async Task<IActionResult> UpdateFooterAddress(UpdateFooterAddressCommand command)
+		{
+			await _mediator.Send(command);
+			return Ok("Güncelleme Başarılı");
+		}
+
+		[HttpDelete]
+		public async Task<IActionResult> RemoveFooterAddress(int id)
+		{
+			await _mediator.Send(new RemoveFooterAdressCommand(id));
+			return Ok("Silme İşlemi Başarılı");
+		}
+	}
+}

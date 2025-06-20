a0da933de14194570bebce7ff13ecc67c400f0a4
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-20 14:19:25 +0300

    .

---
+++ Application/Features/Mediator/Handlers/SocialMediaHandlers/RemoveSocialMediaCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Features.Mediator.Commands.SocialMediaCommands;
+using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Handlers.SocialMediaHandlers
+{
+	public class RemoveSocialMediaCommandHandler : IRequestHandler<RemoveaSocialMediaCommand>
+	{
+		private readonly IRepository<SocialMedia> _repository;
+
+		public RemoveSocialMediaCommandHandler(IRepository<SocialMedia> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(RemoveaSocialMediaCommand request, CancellationToken cancellationToken)
+		{
+			var remove= await _repository.GetByIdAsync(request.Id);	
+			await _repository.RemoveAsync(remove);
+		}
+	}
+}
---
+++ Application/Features/Mediator/Handlers/SocialMediaHandlers/UpdateSocialMediaCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Features.Mediator.Commands.SocialMediaCommands;
+using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Handlers.SocialMediaHandlers
+{
+	public class UpdateSocialMediaCommandHandler : IRequestHandler<UpdateSocialMediaCommand>
+	{
+		private readonly IRepository<SocialMedia> _repository;
+
+		public UpdateSocialMediaCommandHandler(IRepository<SocialMedia> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(UpdateSocialMediaCommand request, CancellationToken cancellationToken)
+		{
+			await _repository.UpdateAsync(new SocialMedia
+			{
+				Icon = request.Icon,
+				Name = request.Name,
+				SocialMediaID = request.SocialMediaID,
+				Url = request.Url,
+
+			});
+		}
+	}
+}

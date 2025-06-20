c52cee6b7989d93da1abbc3b79706feda50ace68
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-20 18:07:26 +0300

    .

---
+++ Application/Features/Mediator/Handlers/TestimonialHandlers/GetTestimonialByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Features.Mediator.Quaries.TestimonialQuaries;
+using Application.Features.Mediator.Results.TestimonialResults;
+using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+	public class GetTestimonialByIdQueryHandler : IRequestHandler<GetTestimonialByIdQuery, GetTestimonialByIdQueryResult>
+	{
+		private readonly IRepository<Testimonial> _repository;
+
+		public GetTestimonialByIdQueryHandler(IRepository<Testimonial> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<GetTestimonialByIdQueryResult> Handle(GetTestimonialByIdQuery request, CancellationToken cancellationToken)
+		{
+			var value=await _repository.GetByIdAsync(request.Id);
+			return new GetTestimonialByIdQueryResult
+			{
+				Comment = value.Comment,
+				ImageUrl = value.ImageUrl,
+				Name = value.Name,
+				TestimonialID = value.TestimonialID,
+				Title = value.Title,
+
+			};
+		}
+	}
+}
---
+++ Application/Features/Mediator/Handlers/TestimonialHandlers/UpdateTestimonialCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Features.Mediator.Commands.TestimonialCommands;
+using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+	public class UpdateTestimonialCommandHandler : IRequestHandler<UpdateTestimonialCommand>
+	{
+		private readonly IRepository<Testimonial>_repository;
+
+		public UpdateTestimonialCommandHandler(IRepository<Testimonial> repository)
+		{
+			_repository = repository;
+		}
+
+		async Task IRequestHandler<UpdateTestimonialCommand>.Handle(UpdateTestimonialCommand request, CancellationToken cancellationToken)
+		{
+			await _repository.UpdateAsync(new Testimonial
+			{
+				Comment = request.Comment,
+				ImageUrl = request.ImageUrl,
+				Name = request.Name,
+				TestimonialID = request.TestimonialID,
+				Title = request.Title,
+			});
+		}
+	}
+}

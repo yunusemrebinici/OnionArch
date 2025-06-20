38ebf63064ead62b0b1da42e4949009bf2f81931
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-20 14:22:33 +0300

    .

---
+++ Application/Features/Mediator/Handlers/SocialMediaHandlers/GetSocialMediaQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Features.Mediator.Quaries.SocialMediaQuaries;
+using Application.Features.Mediator.Results.SocialMediaResults;
+using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Handlers.SocialMediaHandlers
+{
+	public class GetSocialMediaQueryHandler : IRequestHandler<GetSocialMediaQuery, List<GetSocialMediaQueryResult>>
+	{
+		private readonly IRepository<SocialMedia> _repository;
+
+		public GetSocialMediaQueryHandler(IRepository<SocialMedia> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<List<GetSocialMediaQueryResult>> Handle(GetSocialMediaQuery request, CancellationToken cancellationToken)
+		{
+			var values = await _repository.GettAllAsync();
+			return values.Select(x => new GetSocialMediaQueryResult
+			{
+				Icon = x.Icon,
+				Name = x.Name,
+				SocialMediaID = x.SocialMediaID,
+				Url = x.Url,
+			}).ToList();
+		}
+	}
+}

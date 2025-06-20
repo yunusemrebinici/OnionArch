9bf753d41652111353e4c5faa5136cf53fec1470
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-20 17:16:49 +0300

    .

--- CarBookUI/ViewComponents/AboutViewComponent/_AboutUsComponentPartial.cs
+++ CarBookUI/ViewComponents/AboutViewComponent/_AboutUsComponentPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DTO.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBookUI.ViewComponents.AboutViewComponent
 {
 	public class _AboutUsComponentPartial:ViewComponent
 	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
 		public async Task<IViewComponentResult> InvokeAsync()
 		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7217/api/Abouts");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json= await responseMessage.Content.ReadAsStringAsync();
+				var values=JsonConvert.DeserializeObject<List<ResultAboutDto>>(json);
+				return View(values);
+			}
+			
+
 			return View();
 		}
 	}

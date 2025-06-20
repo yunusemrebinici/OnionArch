b2c394f158cf6faa0eed537425fa509dcfe2a9f5
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-20 16:37:47 +0300

    .

---
+++ CarBookUI/ViewComponents/AboutViewComponent/_AboutUsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookUI.ViewComponents.AboutViewComponent
+{
+	public class _AboutUsComponentPartial:ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View();
+		}
+	}
+}

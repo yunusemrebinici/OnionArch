5d6956d871e85054c59319c1815baeaa6bdb070a
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-20 17:39:25 +0300

    .

---
+++ CarBookUI/ViewComponents/BecomeAdriverComponent/_BecomeAdriverComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookUI.ViewComponents.BecomeAdriverComponent
+{
+	public class _BecomeAdriverComponentPartial:ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View();	
+		}
+	}
+}

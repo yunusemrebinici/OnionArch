054e16860b32e1c5c7cbe2098b36744f51a2d4a6
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-21 17:31:10 +0300

    .

---
+++ CarBookUI/Controllers/CarController.cs
@@ -0,0 +1,31 @@
+﻿using DTO.CarDtos;
+using DTO.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookUI.Controllers
+{
+	public class CarController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public CarController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var reponseMessage = await client.GetAsync("https://localhost:7217/api/Cars/GetCarsWithBrand");
+			if (reponseMessage.IsSuccessStatusCode)
+			{
+				var json = await reponseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandDto>>(json);
+				return View(values);
+			}
+
+			return View();
+		}
+	}
+}

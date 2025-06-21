defe1d6a6434c627da19b9d4f065e0b689e897cc
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-21 18:44:34 +0300

    .

--- CarBookUI/Controllers/ContactController.cs
+++ CarBookUI/Controllers/ContactController.cs
@@ -1,11 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DTO.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBookUI.Controllers
 {
 	public class ContactController : Controller
 	{
-		public IActionResult Index()
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public ContactController(IHttpClientFactory httpClientFactory)
 		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(CreateContactDto contactDto)
+		{
+
+			contactDto.SendDate = DateTime.Now;
+			var client = _httpClientFactory.CreateClient();
+			var jsonData= JsonConvert.SerializeObject(contactDto);
+			StringContent content = new StringContent(jsonData, Encoding.UTF8,"application/json");
+
+			var responseMessage = await client.PostAsync("https://localhost:7217/api/Contacts", content);
+			if (responseMessage.IsSuccessStatusCode) { 
+			
+				return RedirectToAction("Index");
+			}
+
 			return View();
 		}
 	}

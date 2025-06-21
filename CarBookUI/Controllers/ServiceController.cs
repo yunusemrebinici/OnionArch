44c85489deaf19b989d1e5faf66224c0fa548dc1
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-21 21:51:09 +0300

    .

--- CarBookUI/Controllers/ServiceController.cs
+++ CarBookUI/Controllers/ServiceController.cs
@@ -6,24 +6,10 @@ namespace CarBookUI.Controllers
 {
 	public class ServiceController : Controller
 	{
-		private readonly IHttpClientFactory _httpClientFactory;
 
-		public ServiceController(IHttpClientFactory httpClientFactory)
-		{
-			_httpClientFactory = httpClientFactory;
-		}
 
 		public async Task<IActionResult> Index()
 		{
-			var client = _httpClientFactory.CreateClient();
-			var reponseMessage = await client.GetAsync("https://localhost:7217/api/Services");
-			if (reponseMessage.IsSuccessStatusCode)
-			{
-				var json = await reponseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(json);
-				return View(values);
-			}
-
 			return View();
 		}
 	}
---
+++ CarBookUI/ViewComponents/ServicesComponent/_ServicesComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using DTO.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookUI.ViewComponents.ServicesComponent
+{
+	public class _ServicesComponentPartial:ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _ServicesComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var reponseMessage = await client.GetAsync("https://localhost:7217/api/Services");
+			if (reponseMessage.IsSuccessStatusCode)
+			{
+				var json = await reponseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(json);
+				return View(values);
+			}
+
+			return View();
+		}
+	}
+}

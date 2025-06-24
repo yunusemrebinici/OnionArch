dae27067301554fce784d573244c05e2848437ef
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-24 17:13:23 +0300

    .

--- CarBookUI/ViewComponents/BlogDetailViewComponent/_BlogAuthorDetailComponentPartial.cs
+++ CarBookUI/ViewComponents/BlogDetailViewComponent/_BlogAuthorDetailComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DTO.BlogDetailDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBookUI.ViewComponents.BlogDetailViewComponent
 {
 	public class _BlogAuthorDetailComponentPartial:ViewComponent
 	{
-		public async Task<IViewComponentResult> InvokeAsync()
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _BlogAuthorDetailComponentPartial(IHttpClientFactory httpClientFactory)
 		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7217/api/Blogs/GetAuthorByBlogId/"+id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json= await responseMessage.Content.ReadAsStringAsync();
+				var value=JsonConvert.DeserializeObject<ResultAuthorByBlogIdDto>(json);
+				return View(value);
+			}
 			return View();
 		}
 	}
---
+++ DTO/BlogDetailDtos/ResultAuthorByBlogIdDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.BlogDetailDtos
+{
+	public class ResultAuthorByBlogIdDto
+	{
+
+		public string name { get; set; }
+
+		public string coverImage { get; set; }
+
+		public string description { get; set; }
+
+
+	}
+}

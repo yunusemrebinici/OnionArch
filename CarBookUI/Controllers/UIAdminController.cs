ee9faffccb8800feb749d2e31654c9c601c16b67
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-25 17:56:37 +0300

    .

---
+++ CarBookUI/Controllers/UIAdminController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookUI.Controllers
+{
+	public class UIAdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public async Task<PartialViewResult> Head()
+		{
+			return PartialView();
+		}
+
+		public async Task<PartialViewResult> Navbar()
+		{
+			return PartialView();
+		}
+
+		public async Task<PartialViewResult> Sidebar()
+		{
+			return PartialView();
+		}
+
+		public async Task<PartialViewResult> Footer()
+		{
+			return PartialView();
+		}
+
+		public async Task<PartialViewResult> Scripts()
+		{
+			return PartialView();
+		}
+
+
+	}
+}

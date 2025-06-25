4064655493783a5349fa218f7437bfca7143b688
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-25 21:35:56 +0300

    .

--- CarBookUI/Controllers/AdminCarController.cs
+++ CarBookUI/Controllers/AdminCarController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DTO.AdminCarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBookUI.Controllers
 {
 	public class AdminCarController : Controller
 	{
-		public IActionResult Index()
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public AdminCarController(IHttpClientFactory httpClientFactory)
 		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7217/api/Cars/GetCarsWithBrand");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCarListWithBrandAdminDto>>(json);
+				return View(values);
+			}
 			return View();
 		}
 	}
---
+++ DTO/AdminCarDtos/ResultCarListWithBrandAdminDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.AdminCarDtos
+{
+	public class ResultCarListWithBrandAdminDto
+	{
+		public int carID { get; set; }
+
+		public string name { get; set; }
+
+		public string model { get; set; }
+
+		public string coverImageUrl { get; set; }
+
+		public decimal carPricing { get; set; }
+
+	}
+}

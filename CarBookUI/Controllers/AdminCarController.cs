c30df18d1f569a6cd4bac0309477c6043bf57a90
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-26 12:54:46 +0300

    .

--- CarBookUI/Controllers/AdminCarController.cs
+++ CarBookUI/Controllers/AdminCarController.cs
@@ -1,6 +1,9 @@
-﻿using DTO.AdminCarDtos;
+﻿using DTO.AdminBrandDtos;
+using DTO.AdminCarDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBookUI.Controllers
 {
@@ -13,7 +16,7 @@ namespace CarBookUI.Controllers
 			_httpClientFactory = httpClientFactory;
 		}
 
-		
+
 		public async Task<IActionResult> Index()
 		{
 			var client = _httpClientFactory.CreateClient();
@@ -30,13 +33,39 @@ namespace CarBookUI.Controllers
 		[HttpGet]
 		public async Task<IActionResult> CreateCar()
 		{
-			
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7217/api/Cars/GetCarsWithBrand");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultBrandInAdminDto>>(json);
+				List<SelectListItem> list = (from x in values
+											 select new SelectListItem
+											 {
+												 Text = x.name,
+												 Value = x.brandID.ToString(),
+											 }).ToList();
+				ViewBag.Brand = list;
+				return View();
+			}
 			return View();
 		}
 
 		[HttpPost]
 		public async Task<IActionResult> CreateCar(CreateCarDto createCar)
 		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createCar);
+			StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7217/api/Cars", content);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+
+				return RedirectToAction("Index");
+
+			}
+
+
 			return View();
 		}
 	}
---
+++ DTO/AdminBrandDtos/ResultBrandInAdminDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.AdminBrandDtos
+{
+	public class ResultBrandInAdminDto
+	{
+
+		public int brandID { get; set; }
+
+		public string name { get; set; }
+
+	}
+}

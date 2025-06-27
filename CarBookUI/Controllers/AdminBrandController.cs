e19861b136935d953647e4a899db20adccd5cdaa
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-27 15:10:38 +0300

    .

---
+++ CarBookUI/Controllers/AdminBrandController.cs
@@ -0,0 +1,96 @@
+﻿using DTO.AdminBrandDtos;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace BrandBookUI.Controllers
+{
+	public class AdminBrandController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		public AdminBrandController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7217/api/Brands");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultBrandInAdminDto>>(json);
+				return View(values);
+			}
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> CreateBrand()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7217/api/Brands/GetBrandsWithBrand");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+	
+			    return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateBrand(CreateBrandAdminDto createBrand)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createBrand);
+			StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7217/api/Brands", content);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+
+			return View();
+		}
+
+
+
+		[HttpGet]
+		public async Task<IActionResult> UpdateBrand(int id)
+		{
+	
+
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7217/api/Brands/" + id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<UpdateBrandAdminDto>(json);
+				return View(values);
+			}
+			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> UpdateBrand(UpdateBrandAdminDto update)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(update);
+			StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PutAsync("https://localhost:7217/api/Brands", content);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+	}
+}
+
---
+++ CarBookUI/Controllers/AdminFeatureController.cs
@@ -0,0 +1,93 @@
+﻿using DTO.AdminFeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBookUI.Controllers
+{
+	public class AdminFeatureController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		public AdminFeatureController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7217/api/Features");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultFeatureAdminDto>>(json);
+				return View(values);
+			}
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> CreateFeature()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7217/api/Features/GetFeaturesWithFeature");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateFeature(CreateFeatureAdminDto createFeature)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createFeature);
+			StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7217/api/Features", content);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+
+			return View();
+		}
+
+
+
+		[HttpGet]
+		public async Task<IActionResult> UpdateFeature(int id)
+		{
+
+
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7217/api/Features/" + id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<UpdateFeatureAdminDto>(json);
+				return View(values);
+			}
+			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> UpdateFeature(UpdateFeatureAdminDto update)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(update);
+			StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PutAsync("https://localhost:7217/api/Features", content);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+
+		}
+	}
+}
+

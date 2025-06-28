ed1f4370f9a7823761252cb186f1a18e368406d5
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-28 12:01:47 +0300

    .

---
+++ DTO/AdminAuthorDtos/CreateAuthorAdminDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.AdminAuthorDtos
+{
+	public class CreateAuthorAdminDto
+	{
+
+		public string Name { get; set; }
+
+		public string CoverImage { get; set; }
+
+		public string Description { get; set; }
+	}
+}
---
+++ DTO/AdminAuthorDtos/UpdateAuthorAdminDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.AdminAuthorDtos
+{
+	public class UpdateAuthorAdminDto
+	{
+		public int AuthorID { get; set; }
+
+		public string Name { get; set; }
+
+		public string CoverImage { get; set; }
+
+		public string Description { get; set; }
+	}
+}

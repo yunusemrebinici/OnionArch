7dbb56745acb3070eff8ab683f6beb68610ca610
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-28 11:12:58 +0300

    .

---
+++ CarBookUI/Areas/Admin/Controllers/AuthorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookUI.Areas.Admin.Controllers
+{
+	public class AuthorController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}

137c0b00527e10cb20ff70b2ffb7bf21487970d1
Yunus Emre Binici <biniciyemre@gmail.com>
2025-06-30 15:30:45 +0300

    .

---
+++ DTO/AdminFooterAddressDtos/CreateFooterAddressAdminDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.AdminFooterAddressDtos
+{
+	public class CreateFooterAddressAdminDto
+	{
+		
+
+		public string Address { get; set; }
+
+		public string Phone { get; set; }
+
+		public string Email { get; set; }
+	}
+}

95b0fffa14703ab4ad9ef284948f042756b857d4
Yunus Emre Binici <biniciyemre@gmail.com>
2025-07-04 14:59:30 +0300

    .

---
+++ CarBookUI/Controllers/CarPricingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookUI.Controllers
+{
+	public class CarPricingController : Controller
+	{
+		public IActionResult Index()
+		{
+			ViewBag.v1 = "Araç Fiyatları";
+			return View();
+		}
+	}
+}

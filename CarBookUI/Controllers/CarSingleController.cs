5e63f1a1dbd954a561be1bd265b6321ea22695b9
Yunus Emre Binici <biniciyemre@gmail.com>
2025-07-08 20:52:57 +0300

    .

---
+++ CarBookUI/Controllers/CarSingleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookUI.Controllers
+{
+	public class CarSingleController : Controller
+	{
+		public IActionResult Index(int id)
+		{
+			ViewBag.v1 = "Detaylar";
+			ViewBag.Id = id;
+			return View();
+		}
+	}
+}
---
+++ CarBookUI/ViewComponents/CarSingleViewComponent/_GetCarDetailComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using DTO.CarSingleDtos;
+using DTO.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookUI.ViewComponents.CarSingleViewComponent
+{
+	public class _GetCarDetailComponentPartial:ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _GetCarDetailComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7217/api/Cars/"+id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<GetCarDetailByIdDto>(json);
+				return View(values);
+			}
+			return View();
+		}
+	}
+}

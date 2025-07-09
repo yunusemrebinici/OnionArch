b063fa8c9ed75be5833d4cbcb627dc1a8d7077d4
Yunus Emre Binici <biniciyemre@gmail.com>
2025-07-09 12:24:15 +0300

    .

--- CarBookUI/ViewComponents/CarSingleViewComponent/_CarSingleDescriptionComponentPartial.cs
+++ CarBookUI/ViewComponents/CarSingleViewComponent/_CarSingleDescriptionComponentPartial.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DTO.CarDescriptionDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBookUI.ViewComponents.CarSingleViewComponent
 {
 	public class _CarSingleDescriptionComponentPartial : ViewComponent
 	{
 
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _CarSingleDescriptionComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
 		public async Task<IViewComponentResult> InvokeAsync(int id)
 		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7217/api/CarDescriptions/GetCarDescriptionByCarId/" + id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<ResultCarDescriptionByCarIdDto>(json);
+				return View(values);
+			}
 			return View();
 		}
 	}
---
+++ DTO/CarDescriptionDtos/ResultCarDescriptionByCarIdDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.CarDescriptionDtos
+{
+	public class ResultCarDescriptionByCarIdDto
+	{
+		public int carDescriptionID { get; set; }
+		public int carID { get; set; }
+		public string details { get; set; }
+	}
+}
